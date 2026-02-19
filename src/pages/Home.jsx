c4358e09e3f7c181627fdc0fc50dd05594ea188c
Daniil Belik <daniilbelik@Daniils-MacBook-Air.local>
2026-02-19 11:12:12 +0100

    fix: Contact form responsiveness and Home layout restructuring

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,9 +14,9 @@ export default function Home() {
             <Hero />
             <Brands />
             <Services />
+            <WhyUs />
             <Process />
             <About />
-            <WhyUs />
             <Testimonials />
             <FAQ />
             <Contact />
